1fb4cddebea45e181b593791f43f254550ed83aa
utsabsh <np03cs4a220209@heraldcollege.edu.np>
2024-04-07 14:59:02 +0545

    feat:Addemployee

--- hrms/src/Components/AddEmployee.jsx
+++ hrms/src/Components/AddEmployee.jsx
@@ -1,9 +1,180 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddEmployee = () => {
-  return (
-    <div>AddEmployee</div>
-  )
-}
+  const [employee, setEmployee] = useState({
+    name: "",
+    email: "",
+    password: "",
+    salary: "",
+    address: "",
+    category_id: "1",
+    image: "",
+  });
+  const [category, setCategory] = useState([]);
+  const navigate = useNavigate();
 
-export default AddEmployee
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/auth/category")
+      .then((result) => {
+        if (result.data.Status) {
+          setCategory(result.data.Result);
+        } else {
+          alert(result.data.Error);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", employee.name);
+    formData.append("email", employee.email);
+    formData.append("password", employee.password);
+    formData.append("address", employee.address);
+    formData.append("salary", employee.salary);
+    formData.append("image", employee.image);
+    formData.append("category_id", employee.category_id);
+    console.log(employee);
+
+    axios
+      .post("http://localhost:3000/auth/add_employee", formData)
+      .then((result) => {
+        if (result.data.status) {
+          navigate("/dashboard/employee");
+        } else {
+          alert(result.data.Error);
+          console.log(result.data.Error);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className="flex justify-center items-center mt-3">
+      <div className="p-3 rounded w-96 border">
+        <h3 className="text-center">Add Employee</h3>
+        <form className="grid grid-cols-1 gap-4" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="inputName" className="block">
+              Name
+            </label>
+            <input
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="inputName"
+              placeholder="Enter Name"
+              onChange={(e) =>
+                setEmployee({ ...employee, name: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="inputEmail4" className="block">
+              Email
+            </label>
+            <input
+              type="email"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="inputEmail4"
+              placeholder="Enter Email"
+              autoComplete="off"
+              onChange={(e) =>
+                setEmployee({ ...employee, email: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="inputPassword4" className="block">
+              Password
+            </label>
+            <input
+              type="password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="inputPassword4"
+              placeholder="Enter Password"
+              onChange={(e) =>
+                setEmployee({ ...employee, password: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="inputSalary" className="block">
+              Salary
+            </label>
+            <input
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="inputSalary"
+              placeholder="Enter Salary"
+              autoComplete="off"
+              onChange={(e) =>
+                setEmployee({ ...employee, salary: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="inputAddress" className="block">
+              Address
+            </label>
+            <input
+              type="text"
+              className="hadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="inputAddress"
+              placeholder="1234 Main St"
+              autoComplete="off"
+              onChange={(e) =>
+                setEmployee({ ...employee, address: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="category" className="block">
+              Category
+            </label>
+            <select
+              name="category_id"
+              id="category"
+              className="form-select"
+              onChange={(e) =>
+                setEmployee({ ...employee, category_id: e.target.value })
+              }
+            >
+              {category.map((c) => (
+                <option key={c.id} value={c.id}>
+                  {c.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label className="block" htmlFor="inputGroupFile01">
+              Select Image
+            </label>
+            <input
+              type="file"
+              className="form-input rounded-none"
+              id="inputGroupFile01"
+              name="image"
+              onChange={(e) =>
+                setEmployee({ ...employee, image: e.target.files[0] })
+              }
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white rounded-full py-2 px-4 w-full"
+            >
+              Add Employee
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddEmployee;
