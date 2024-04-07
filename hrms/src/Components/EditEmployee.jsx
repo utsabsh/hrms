e43aefe2cfa39d575a1193981e91765f3c83bd7f
sulop-maharjan <np03cs4a220012@heraldcollege.edu.np>
2024-04-07 15:03:32 +0545

    feat:edit employee

--- hrms/src/Components/EditEmployee.jsx
+++ hrms/src/Components/EditEmployee.jsx
@@ -1,8 +1,127 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EditEmployee = () => {
+    const {id} = useParams()
+    const [employee, setEmployee] = useState({
+        name: "",
+        email: "",
+        salary: "",
+        address: "",
+        category_id: "",
+      });
+      const [category, setCategory] = useState([])
+      const navigate = useNavigate()
+
+      useEffect(()=> {
+        axios.get('http://localhost:3000/auth/category')
+        .then(result => {
+            if(result.data.Status) {
+                setCategory(result.data.Result);
+            } else {
+                alert(result.data.Error)
+            }
+        }).catch(err => console.log(err))
+
+        axios.get('http://localhost:3000/auth/employee/'+id)
+        .then(result => {
+            setEmployee({
+                ...employee,
+                name: result.data.Result[0].name,
+                email: result.data.Result[0].email,
+                address: result.data.Result[0].address,
+                salary: result.data.Result[0].salary,
+                category_id: result.data.Result[0].category_id,
+            })
+        }).catch(err => console.log(err))
+    }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.put('http://localhost:3000/auth/edit_employee/'+id, employee)
+        .then(result => {
+            if(result.data.Status) {
+                navigate('/dashboard/employee')
+            } else {
+                alert(result.data.Error)
+            }
+        }).catch(err => console.log(err))
+    }
+    
   return (
-    <div>EditEmployee</div>
+    <div className="flex justify-center items-center mt-3">
+    <div className="p-3 rounded border w-1/2">
+      <h3 className="text-center">Edit Employee</h3>
+      <form className="grid grid-cols-1 gap-4" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="inputName" className="block">Name</label>
+          <input
+            type="text"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="inputName"
+            placeholder="Enter Name"
+            value={employee.name}
+            onChange={(e) => setEmployee({ ...employee, name: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="inputEmail4" className="block">Email</label>
+          <input
+            type="email"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="inputEmail4"
+            placeholder="Enter Email"
+            autoComplete="off"
+            value={employee.email}
+            onChange={(e) => setEmployee({ ...employee, email: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="inputSalary" className="block">Salary</label>
+          <input
+            type="text"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="inputSalary"
+            placeholder="Enter Salary"
+            autoComplete="off"
+            value={employee.salary}
+            onChange={(e) => setEmployee({ ...employee, salary: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="inputAddress" className="block">Address</label>
+          <input
+            type="text"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="inputAddress"
+            placeholder="1234 Main St"
+            autoComplete="off"
+            value={employee.address}
+            onChange={(e) => setEmployee({ ...employee, address: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="category" className="block">Category</label>
+          <select
+            name="category"
+            id="category"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            onChange={(e) => setEmployee({ ...employee, category_id: e.target.value })}
+          >
+            {category.map((c) => (
+              <option key={c.id} value={c.id}>{c.name}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-full rounded">
+            Edit Employee
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
   )
 }
 
