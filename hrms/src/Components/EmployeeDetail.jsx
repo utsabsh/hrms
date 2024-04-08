c866c6eb3d81dcbcc5df8e9f0aba857f58df45c0
sulop-maharjan <np03cs4a220012@heraldcollege.edu.np>
2024-04-08 07:28:32 +0545

    feat:EmployeeDetails

--- hrms/src/Components/EmployeeDetail.jsx
+++ hrms/src/Components/EmployeeDetail.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const EmployeeDetail = () => {
-  return <div>EmployeeDetail</div>;
+  const [employee, setEmployee] = useState([]);
+  const { id } = useParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/employee/detail/" + id)
+      .then((result) => {
+        setEmployee(result.data[0]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  const handleLogout = () => {
+    axios
+      .get("http://localhost:3000/employee/logout")
+      .then((result) => {
+        if (result.data.Status) {
+          localStorage.removeItem("valid");
+          navigate("/");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div>
+      <div className="p-2 flex justify-center shadow">
+        <h4 className="text-center">Employee Management System</h4>
+      </div>
+      <div className="flex justify-center flex-col items-center mt-3">
+        <img
+          src={`http://localhost:3000/Images/${employee.image}`}
+          className="emp_det_image"
+        />
+        <div className="flex flex-col items-center mt-5">
+          <h3>Name: {employee.name}</h3>
+          <h3>Email: {employee.email}</h3>
+          <h3>Salary: ${employee.salary}</h3>
+        </div>
+        <div className="mt-3">
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EmployeeDetail;
